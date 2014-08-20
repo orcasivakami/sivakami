97e62c627adfd0822e46ff82180e1ae53ae93fe4
orcasivakami <sivakami.ctrmvisa@gmail.com>
2014-08-20 11:31:10 +0530

    searching

---
+++ search.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main (void)
+{
+	int len, find_val;
+	int j,k;
+	_Bool isFound="false";
+	printf ("Length of the array:");
+	scanf ("%i", &len);
+	int a[len];
+	printf("Enter the array data:");
+	for(j=0;j<len;j++){
+		scanf("%i",&a[j]);
+	}
+/*	printf("The array data:");
+	for(j=0;j<len;j++){
+		printf("\nThe array data at %i ",j);
+		printf("th position is %i",a[j]);
+	}*/
+	printf ("Enter the value to found in the array:");
+	scanf ("%i", &find_val);
+	for(j=0;j<len;j++){
+		if(find_val==a[j]){
+			isFound="true";
+			printf("\nThe value %i is found at the array position %i", find_val,j);				
+		}
+	}
+	if(!isFound){
+		printf("Value not found");
+	}
+return 0;
+}
