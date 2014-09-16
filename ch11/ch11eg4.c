f44ed87eae573a0a61d71aac2e64e08221fdfd5a
orcasivakami <sivakami.ctrmvisa@gmail.com>
2014-09-16 16:13:27 +0530

    chapter 11

---
+++ ch11/ch11eg4.c
@@ -0,0 +1,23 @@
+/* Program to illustrate structure pointers
+*  by Sivakami, Sep 2014
+*/
+#include <stdio.h>
+
+int main (void)
+{
+	struct date
+	{
+		int month;
+		int day;
+		int year;
+	};
+	
+	struct date today, *datePtr;
+	datePtr = &today;
+	datePtr->month = 9;
+	datePtr->day = 25;
+	datePtr->year = 2004;
+	printf ("Today's date is %i/%i/%.2i.\n",datePtr->month, datePtr->day, datePtr->year % 100);
+	return 0;
+}
+
